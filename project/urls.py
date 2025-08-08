920c5f08ad68930fef45b6596ead556781544f9c
Joemugare <134427671+Joemugare@users.noreply.github.com>
2025-08-08 08:33:20 +0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,41 +1,34 @@
-﻿"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from itertools import product
+Project URL Configuration
+
+For more information:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+"""
+
 from django.contrib import admin
-from django.shortcuts import redirect  # Add this import at the top
-from django.urls import path , include
+from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
-from .settings import MEDIA_ROOT
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
-    path('', lambda request: redirect('products/', permanent=False)),  # 👈 This is the fix
+    path('', lambda request: redirect('products/', permanent=False)),
     path('admin/', admin.site.urls),
     path('products/', include('products.urls', namespace='products')),
-    path('__debug__/', include('debug_toolbar.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
 ]
 
-
+# Only include debug toolbar in development
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-    urlpatterns += static(settings. MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+
+# Serve static and media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
